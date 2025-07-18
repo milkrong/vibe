0c957da2ec65a6b4c4227285e0854d7659e98649
rk_liu <rk_liu@trip.com>
2025-07-18 09:52:58 +0800

    02: db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,19 @@
-const Home = () => {
-  return <div className="text-3xl font-bold underline">Home</div>;
-};
+import { prisma } from '../lib/db';
 
-export default Home;
+export default async function Home() {
+  const users = await prisma.user.findMany();
+  return (
+    <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center -mt-16">
+      <h1 className="text-4xl font-bold mb-8 font-[family-name:var(--font-geist-sans)] text-[#333333]">
+        Superblog
+      </h1>
+      <ol className="list-decimal list-inside font-[family-name:var(--font-geist-sans)]">
+        {users.map((user) => (
+          <li key={user.id} className="mb-2">
+            {user.name}
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
+}
