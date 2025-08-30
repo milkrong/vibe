c87ef5f834823673cab9854af7472f35b177dd34
milkrong <milkrong121@outlook.com>
2025-08-30 16:00:22 +0800

    feat: enhance home page with input handling and update invoke mutation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,12 @@
 import { Button } from '@/components/ui/button';
 import { useTRPC } from '@/trpc/client';
 import { useMutation } from '@tanstack/react-query';
+import { useState } from 'react';
 import { toast } from 'sonner';
 
 export default function Home() {
   const trpc = useTRPC();
+  const [input, setInput] = useState('');
   const invoke = useMutation(
     trpc.invoke.mutationOptions({
       onSuccess: () => {
@@ -20,9 +22,14 @@ export default function Home() {
       <h1 className="text-4xl font-bold mb-8 font-[family-name:var(--font-geist-sans)] text-[#333333]">
         Super blog
       </h1>
+      <input
+        type="text"
+        className="border-2 border-gray-300 rounded-md p-2"
+        onChange={(e) => setInput(e.target.value)}
+      />
       <Button
         disabled={invoke.isPending}
-        onClick={() => invoke.mutate({ test: 'world' })}
+        onClick={() => invoke.mutate({ test: input })}
       >
         Invoke
       </Button>
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,22 @@
 import { inngest } from './client';
+import { createAgent, openai } from '@inngest/agent-kit';
 
 export const helloWorld = inngest.createFunction(
   { id: 'hello-world' },
   { event: 'test/hello.world' },
-  async ({ event, step }) => {
-    await step.sleep('wait-a-moment', '1s');
-    return { message: `Hello ${event.data.email}!` };
+  async ({ event }) => {
+    console.log(process.env.SILICONFLOW_API_KEY);
+    const agent = createAgent({
+      name: 'Summarize',
+      system: 'You are a helpful assistant that summarizes text',
+      model: openai({
+        model: 'Pro/deepseek-ai/DeepSeek-V3.1',
+        baseUrl: 'https://api.siliconflow.cn/v1/',
+        apiKey: process.env.SILICONFLOW_API_KEY,
+      }),
+    });
+    const { output } = await agent.run(event.data.email);
+    console.log(output);
+    return output;
   }
 );
